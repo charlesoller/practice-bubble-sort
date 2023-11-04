ea4b10676098d029b9ccb37251532924fcbba6cd
charlesoller <charlesrello@gmail.com>
2023-11-04 13:16:31 -0400

    finished

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,18 +1,22 @@
 
 function bubbleSort(arr) {
-
+  let isSwapped = true;
     // Iterate through the array
-
+    while(isSwapped){
+      isSwapped = false;
+      for(let i = 0; i < arr.length; i++){
       // If the current value is greater than its neighbor to the right
         // Swap those values
-
-        // Do not move this console.log
-        console.log(arr.join(","));
-
+        if(arr[i] > arr[i+1]){
+          [arr[i], arr[i+1]] = [arr[i+1], arr[i]]
+          // Do not move this console.log
+          console.log(arr.join(","));
+          isSwapped = true;
+        }
     // If you get to the end of the array and no swaps have occurred, return
-
+      }
+    }
     // Otherwise, repeat from the beginning
-
 }
 
 module.exports = bubbleSort;
